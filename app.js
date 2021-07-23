85f9806d48a329f2027697bf2f617af223b38d52
Promise <onyekamorka06@gmail.com>
2021-07-22 23:25:32 -0400

    Added two JavaScript  events to a button

--- app.js
+++ app.js
@@ -1,78 +1,16 @@
-// Returning an anonymous function as a value of another function
-function greet(greeting) {
-	return function (name) {
-		return `${greeting}, ${name}.`;
-	};
+// function to make color
+function makeColor(color) {
+	document.body.style.backgroundColor = color;
 }
 
-//This method is basically capturing the return value (i.e another function) in a variable
-// const firstCall = greet("Hello");
-// console.log(firstCall("Promise"));
-// This other method is calling both functions at once
-console.log(greet("Hello")("Onyeka"));
-
-console.log(`=====================`);
-
-function markWahlberg(animal, callback) {
-	console.log(`Hey ${animal}, how you doin'?`);
-	callback();
-}
-
-function marksCallback() {
-	console.log(`Say hi to your mother for me, alright?`);
-}
-
-markWahlberg("dog", marksCallback);
-
-function doMath(a, b, callback) {
-	return callback(a, b);
-}
-
-const output = doMath(4, 3, function (firstParameter, secondParameter) {
-	return firstParameter + secondParameter;
+const redBtn = document.querySelector("#orangered");
+// Added the first event listener to the button
+redBtn.addEventListener("click", function () {
+	makeColor("orangered");
 });
 
-console.log(output);
-
-function greetILoveYou() {
-	console.log(`Hi, I love you!`);
-}
-
-function diss() {
-	console.log(`You are the worse`);
-}
-
-// Putting functions inside an array and destructuring the array
-let [funcs1, funcs2] = [greetILoveYou, diss];
-funcs1();
-funcs2();
-
-function repeatThreeTimes(func) {
-	func();
-	func();
-	func();
-}
-
-console.log(repeatThreeTimes(greetILoveYou));
-
-// RETURNING FUNCTION FROM A ANOTHER FUCNTION AND CALLING THEM
-function giveBirth() {
-	console.log("GIVING BIRTH");
-	return function cry() {
-		return "WAAAAAAHHH";
-	};
-}
-
-// You can call both functions like this
-console.log(giveBirth()());
-
-// or you can store the output in a variable and call them
-const theFirstCall = giveBirth();
-console.log(theFirstCall());
-
-greetILoveYou();
-console.log(setTimeout(diss, 3000));
-console.log(setInterval(diss, 3000));
-console.log(setTimeout(diss, 3000));
-
-greetILoveYou();
+// Added the second event listener to the button
+const h1 = document.querySelector("h1");
+redBtn.addEventListener("click", function () {
+	h1.style.color = "yellow";
+});
