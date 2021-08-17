cc5fe243ebe2728537a81364dabbfa2447462204
Promise Morka <onyekamorka06@gmail.com>
2021-08-17 09:10:06 -0400

    Practiced rest and spread method

--- app.js
+++ app.js
@@ -1,16 +1,70 @@
-// function to make color
-function makeColor(color) {
-	document.body.style.backgroundColor = color;
+// OLD WAYS OF DOING THINGS
+function max() {
+	console.log(arguments);
 }
 
-const redBtn = document.querySelector("#orangered");
-// Added the first event listener to the button
-redBtn.addEventListener("click", function () {
-	makeColor("orangered");
-});
+max(2, 3, 4);
 
-// Added the second event listener to the button
-const h1 = document.querySelector("h1");
-redBtn.addEventListener("click", function () {
-	h1.style.color = "yellow";
-});
+function max() {
+	const arr = Array.from(arguments);
+	return arr.reduce((acc, curValue) => {
+		return curValue > acc ? curValue : acc;
+		// if (curValue > acc) {
+		// 	return curValue;
+		// } else {
+		// 	return acc;
+		// }
+		// return acc;
+	});
+}
+
+console.log(max(2, 3, 5, 894));
+
+// NEW WAYS OF DOING THINGS
+function makeFamily(parent1, parent2, ...children) {
+	// console.log(parent1, parent2, children);
+	const familyObj = {};
+	familyObj.mother = parent1;
+	familyObj.father = parent2;
+	familyObj.kids = children;
+	return familyObj;
+}
+console.log(makeFamily("cindy", "john", "cody", "carly"));
+console.log(makeFamily("cindy", "john"));
+
+function takesFour(one, two, three, four) {
+	console.log(one);
+	console.log(two);
+	console.log(three);
+	console.log(four);
+}
+
+const names = ["Mary", "Colt", "Angela", "Abe"];
+takesFour(...names);
+
+// Slice method does not change the array
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const anotherNums = nums.slice(3);
+console.log(anotherNums);
+console.log(nums);
+
+// Splice changes the array
+const numsTwo = [9, 10, 11, 12, 13, 14, 15];
+const anotherNumsTwo = numsTwo.splice(3);
+console.log(anotherNumsTwo);
+console.log(numsTwo);
+
+const vowels = "aeiou";
+const oldVowelArr = vowels.split("");
+const newVowelAarr = [...vowels];
+
+const car = {
+	brand: "Mercedes",
+	year: 2020,
+	color: "blue",
+};
+
+const carArr = ["Mercedes", 2020, "blue"];
+
+const carObject = { ...car };
+const carArray = { ...carArr };
