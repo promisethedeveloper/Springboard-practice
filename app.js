22006ee022b2cdfab991be08c39cd947b0a07ac6
Promise Morka <onyekamorka06@gmail.com>
2021-08-29 01:24:07 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -1,16 +1,85 @@
-// function to make color
-function makeColor(color) {
-	document.body.style.backgroundColor = color;
+// ADVANCED ARRAY METHODS
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function forEach(array, callback) {
+	for (let i = 0; i < array.length; i++) {
+		callback(array[i], i, array);
+	}
 }
 
-const redBtn = document.querySelector("#orangered");
-// Added the first event listener to the button
-redBtn.addEventListener("click", function () {
-	makeColor("orangered");
+function countZeros(arr) {
+	let total = 0;
+	arr.forEach((el) => {
+		if (el === 0) {
+			total++;
+		}
+	});
+	return total;
+}
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+function doMath(a, b, func) {
+	return func(a, b);
+}
+
+console.log(doMath(5, 6, add));
+
+doMath(3, 4, function (a, b) {
+	console.log(a ** b);
 });
 
-// Added the second event listener to the button
-const h1 = document.querySelector("h1");
-redBtn.addEventListener("click", function () {
-	h1.style.color = "yellow";
+const mathFunc = [add, multiply, subtract, divide];
+
+function doAllMath(a, b, func) {
+	for (const x of func) {
+		let values = x(a, b);
+		console.log(values);
+	}
+}
+
+doAllMath(20, 20, mathFunc);
+
+numbers.forEach((el) => console.log(el));
+
+const colors = ["blue", "red", "purple", "cyan", "brown"];
+colors.forEach((el) => {
+	console.log(el.toUpperCase());
+});
+
+// personal forEach method
+const friends = ["Ugo", "Jo", "Jack", "Abel"];
+
+function myForEach(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		callback(arr[i], i);
+	}
+}
+
+function callBackFunc(value, position) {
+	console.log(
+		`${value} is a great guy! His current position is at index: ${position}`
+	);
+	// return undefined;
+}
+
+myForEach(friends, callBackFunc);
+
+// regular forEach method
+friends.forEach((el, i) => {
+	console.log(`${el} is a great guy! His current position is at index: ${i}`);
 });
