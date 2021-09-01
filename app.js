78923914943f3380fd7a9b2e4764bf5cf50a8579
Promise Morka <onyekamorka06@gmail.com>
2021-08-31 23:46:52 -0400

    added some and every methods

--- app.js
+++ app.js
@@ -249,3 +249,256 @@ const n = [0, 2, 4, 5, 6, 7, 8];
 const evens = myFilter(n, function (n) {
 	return n % 2 === 0;
 });
+
+// SOME
+
+let digits = [1, 2, 3];
+
+const a = digits.some(function (value, index, array) {
+	return value < 3;
+});
+console.log(a);
+
+function some(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr) === true) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function hasAdmin(arr) {
+	return arr.some((el) => {
+		return el.admin;
+	});
+}
+
+console.log(
+	hasAdmin([
+		{ name: "Colt", admin: true },
+		{ name: "Poppy", admin: false },
+	])
+);
+
+console.log(hasAdmin([{ name: "Colt" }, { name: "Poppy", admin: false }]));
+
+function hasQuestionMark(str) {
+	return str.split("").some((el) => {
+		return el === "?";
+	});
+}
+
+console.log(hasQuestionMark("How are you feeling"));
+console.log(hasQuestionMark("How are you feeling now?"));
+
+let xyz = [1, 2, 3, 4, 5];
+const yz = xyz.every((el) => {
+	return el >= 1;
+});
+console.log(yz);
+
+function myEvery(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr) === false) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function allVowels(str) {
+	return str.split("").every(function (el) {
+		return "aeiou".includes(el);
+	});
+}
+
+function isInteger(arr) {
+	return arr.every((el) => {
+		return Number.isInteger(el);
+	});
+}
+console.log(isInteger([1, 2, 3, 4, 5, 2.5]), "answer");
+
+const someResult = function (arr) {
+	return arr.some((el) => {
+		// return el.includes("thyroid");
+		return el.indexOf("thyroid") !== -1;
+	});
+};
+
+console.log(someResult(words), "some");
+
+const lengthOfTheWordsArray = function (arr) {
+	return arr.every((el) => {
+		return el.length >= 5;
+	});
+};
+
+console.log(lengthOfTheWordsArray(words), "every");
+
+const everyElementAString = (arr) => arr.every((el) => typeof el === "string");
+
+console.log(everyElementAString(words), "every");
+
+const allInputs = document.querySelectorAll("#terms input");
+const btn = document.querySelector("button");
+btn.addEventListener("click", (e) => {
+	const allCheckedBoxes = [...allInputs].every((el) => {
+		return el.checked;
+	});
+	if (!allCheckedBoxes) {
+		alert("Please agree to everything");
+	}
+	console.log(allCheckedBoxes);
+});
+
+const mySome = (arr, callback) => {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr)) {
+			return true;
+		}
+	}
+	return false;
+};
+
+console.log(
+	mySome([1, 2, 3, 4, 5], function (el) {
+		return el > 2;
+	})
+);
+
+const anotherSum = (arr, callback) => {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr) === false) {
+			return false;
+		}
+	}
+	return true;
+};
+
+console.log(
+	anotherSum([1, 2, 3, 4, 5], function (el) {
+		return el >= 1;
+	})
+);
+
+const things = [1, 2, 3, 4, 5, 6];
+
+function theFind(arr) {
+	return arr.find((el) => {
+		return el > 2;
+	});
+}
+
+console.log(theFind(things), "find");
+
+function myFind(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr) === true) {
+			return arr[i];
+		}
+	}
+	return undefined;
+}
+
+function findOddNumber(arr) {
+	return arr.find(function (el) {
+		return el % 2 !== 0;
+	});
+}
+
+const b = (arr) => {
+	return arr.findIndex((el) => {
+		return el > 5;
+	});
+};
+
+console.log(b(things));
+
+function myFindIndex(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr)) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+const findPositionOfBoolean = (arr) => {
+	return arr.findIndex((el) => {
+		return typeof el === "boolean";
+	});
+};
+console.log(findPositionOfBoolean([1, 3, false, true]));
+console.log(findPositionOfBoolean(["no booleans", "around", "these parts"]));
+
+const scores = [
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 68, 75, 78, 90, 99, 46, 66, 37, 79, 77,
+];
+
+const xx = (arr) => arr.find((score) => score > 75);
+
+const firstEvenScore = (arr) => arr.find((el) => el % 2 === 0 && el !== 0);
+
+console.log(xx(scores));
+console.log(firstEvenScore(scores));
+
+const getAllEvenScores = (arr) => arr.filter((el) => el % 2 === 0 && el !== 0);
+
+console.log(getAllEvenScores(scores));
+
+const indexOfFirstEvenNum = (arr) =>
+	arr.findIndex((el) => el % 2 === 0 && el !== 0);
+
+console.log(indexOfFirstEvenNum(scores));
+
+function partition(arr, pivot) {
+	const pivotIndex = arr.findIndex((el) => {
+		return el > pivot;
+	});
+	const left = arr.slice(0, pivotIndex);
+	const right = arr.slice(pivotIndex);
+	return [left, right];
+}
+
+console.log(partition(scores, 0));
+
+const partitionArrow = (arr, pivot) => {
+	const pivotIndex = arr.findIndex((el) => el > pivot);
+	const left = arr.slice(0, pivotIndex);
+	const right = arr.slice(pivotIndex);
+	return [left, right];
+};
+
+console.log(partitionArrow(scores, 75));
+
+function latestMyFind(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr)) {
+			return arr[i];
+		}
+	}
+	return undefined;
+}
+
+console.log(
+	latestMyFind(scores, function (el) {
+		return el > 0;
+	})
+);
+
+function latestMyFindIndex(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr)) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+console.log(
+	latestMyFindIndex(scores, function (el) {
+		return el > 0;
+	})
+);
